8b98392cf4c711b9c1262ece5ac4a8549fb88689
Roman Tolstosheyev <izobretatel777@mail.ru>
2022-11-28 11:57:05 +0400

    Revert "Add profile menu"
    This reverts commit e9baed3d730cab8c574361f104e11795122b20af.
    
    # Conflicts:
    #	src/main/java/az/izobretatel7777/hackersuitcase/controller/LoginController.java
    #	src/main/java/az/izobretatel7777/hackersuitcase/service/ProfileService.java
    #	src/main/java/az/izobretatel7777/hackersuitcase/service/implementations/ProfileServiceImpl.java
    #	src/main/resources/templates/header.html
    #	src/main/resources/templates/profile.html

--- src/main/java/az/izobretatel7777/hackersuitcase/service/ProfileService.java
+++
@@ -1,8 +0,0 @@
-package az.izobretatel7777.hackersuitcase.service;
-
-import az.izobretatel7777.hackersuitcase.dao.entity.User;
-
-public interface ProfileService {
-    boolean updateProfile(User user);
-    User getProfile();
-}
